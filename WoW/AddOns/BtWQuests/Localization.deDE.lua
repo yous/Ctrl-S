bb2eb6f6e926015648f84192553587d92deba401
Chayoung You <yousbe@gmail.com>
2021-04-29 00:59:27 +0900

    Add BtWQuests add ons

---
+++ WoW/AddOns/BtWQuests/Localization.deDE.lua
@@ -0,0 +1,43 @@
+----- AUTO GENERATED - DO NOT EDIT
+
+if GetLocale() ~= "deDE" then
+    return
+end
+
+local L = BtWQuests.L
+L["BTWQUESTS_ACTIVE"] = "Aktiv"
+L["BTWQUESTS_ALLIED_RACE"] = "Verbündete Völker"
+L["BTWQUESTS_ALLIED_RACES"] = "Verbündete Völker"
+L["BTWQUESTS_AVAILABLE"] = "Verfügbar"
+L["BTWQUESTS_COLLECT"] = "Sammle %s"
+L["BTWQUESTS_COMPLETED"] = "Abgeschlossen"
+L["BTWQUESTS_COSMETIC"] = "Kosmetisch"
+L["BTWQUESTS_COUNTDOWN_DHM"] = "%d Tage, %d Stunden und %d Minuten"
+L["BTWQUESTS_EPILOGUE"] = "Epilog"
+L["BTWQUESTS_EQUIP"] = "Rüste %s aus"
+L["BTWQUESTS_FACTION"] = "Fraktion"
+L["BTWQUESTS_IGNORED"] = "Ignoriert"
+L["BTWQUESTS_LEVEL"] = "Stufe %d"
+L["BTWQUESTS_MOUNT"] = "Aufsitzen"
+L["BTWQUESTS_PET"] = "Begleiter"
+L["BTWQUESTS_PROFESSIONS"] = "Berufe"
+L["BTWQUESTS_PROFESSION_ALCHEMY"] = "Alchemie"
+L["BTWQUESTS_PROFESSION_BLACKSMITHING"] = "Schmiedekunst"
+L["BTWQUESTS_PROFESSION_ENCHANTING"] = "Verzauberkunst"
+L["BTWQUESTS_PROFESSION_ENGINEERING"] = "Ingenieurskunst"
+L["BTWQUESTS_PROFESSION_FIRST_AID"] = "Erste Hilfe"
+L["BTWQUESTS_PROFESSION_FISHING"] = "Angeln"
+L["BTWQUESTS_PROFESSION_HERBALISM"] = "Kräuterkunde"
+L["BTWQUESTS_PROFESSION_INSCRIPTION"] = "Inschriftenkunde"
+L["BTWQUESTS_PROFESSION_JEWELCRAFTING"] = "Juwelierskunst"
+L["BTWQUESTS_PROFESSION_LEATHERWORKING"] = "Lederverarbeitung"
+L["BTWQUESTS_PROFESSION_MINING"] = "Bergbau"
+L["BTWQUESTS_PROFESSION_SKINNING"] = "Kürschnerei"
+L["BTWQUESTS_PROFESSION_TAILORING"] = "Schneiderei"
+L["BTWQUESTS_PROLOGUE"] = "Prolog"
+L["BTWQUESTS_TOY"] = "Spielzeug"
+L["BTWQUESTS_WORLD_QUESTS"] = "Weltquests"
+L["OTHER"] = "Anderes"
+L["RANK"] = "%s Rang %d"
+L["SHOW_MINIMAP_ICON"] = "Zeige Minikarten-Symbol"
+L["UNKNOWN"] = "Unbekannt"
